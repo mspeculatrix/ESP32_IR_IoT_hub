e2261268d5d879c9739dd5c2b18cc0c576056c37
mspeculatrix <smd@webvivant.com>
2019-09-23 11:24:34 +0200

    Create wifi-credentials-template.h

---
+++ wifi-credentials-template.h
@@ -0,0 +1,15 @@
+/* Wifi credentials
+	
+	Edit this file to match your wifi environment.
+	
+	Put as many SSIDs as you want in the ssid array (it can contain
+	just one, if that's all you have). This assumes the password is
+	the same for all access points.
+
+	Once you've made the edits, rename (or 'save as...') this file
+	with the filename: wifi-credentials.h in the same directory as
+	the main sketch.
+*/
+
+#define WLAN_PASS      "your_password"
+const char* ssid [] = { "your_SSID_1", "your_SSID_2" };
